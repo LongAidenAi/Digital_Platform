f3ccf0e476845b6c3ce35bf20a5ccf763a8f5f1a
LongAidenAi <woai032867@gmail.com>
2023-09-24 20:32:41 +0800

    2.10 响应JSON格式的书籍

--- src/main.js
+++ src/main.js
@@ -1,21 +1,21 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-    switch (request.url) {
-        case '/':
-            response.write('hello ~');
-            break;
-        case '/posts': 
-            response.write("posts ~");
-            break;
-        case "/signup":
-            response.write("signup");
-            break;
-        default:
-            response.writeHead(404);
-            response.write("404");
-            break;
+    const data = {
+        id: 1,
+        title: "amind",
+        content:"明月出天山，苍茫云海间"
     }
+
+    const jsonData = JSON.stringify(data);
+
+    //解析中文字
+    response.writeHead(200, {
+        "Content-Type": "application/json; charset=utf-8"
+    })
+
+    response.write(jsonData);
+
     response.end();
 })
 
