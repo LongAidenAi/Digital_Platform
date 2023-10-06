74b92a9f42e19eca9914d20e26a316a52930990c
LongAidenAi <woai032867@gmail.com>
2023-10-06 19:12:37 +0800

    10.11 路由：Router

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,5 +1,6 @@
 
 import express from 'express'
+import postRouter from '../post/post.router'
 
 /**
  * 创建应用
@@ -10,6 +11,12 @@ const app = express();
  * 处理JSON
  */
 app.use(express.json())
+
+
+/**
+ * 路由
+ */
+app.use(postRouter)
 
 /**
  * 导出应用
