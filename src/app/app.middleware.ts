8233479765d0854393edd16472509b05fd597ab8
LongAidenAi <woai032867@gmail.com>
2023-10-06 19:33:04 +0800

    10.16 创建默认异常处理器

--- src/app/app.middleware.ts
+++ src/app/app.middleware.ts
@@ -11,3 +11,27 @@ export const requestUrl = (
     console.log(request.url)
     next()
 }
+
+/**
+ * 默认异常处理器
+ */
+export const defaultErrorHandler = (
+    error: any,
+    request: Request,
+    response: Response,
+    next: NextFunction
+) => {
+    let statusCode: number, message: string;
+
+    /**
+     * 处理异常
+     */
+    switch (error.message) {
+        default:
+            statusCode = 500;
+            message = "服务器出现了一点问题"
+            break
+    }
+
+    response.status(statusCode).send({ message })
+}
