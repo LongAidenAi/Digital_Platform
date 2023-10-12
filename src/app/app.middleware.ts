88b0ffeb43c0cc302c7d18be930a3b9c57d0587b
LongAidenAi <woai032867@gmail.com>
2023-10-12 16:28:54 +0800

    12.12 对使用数据服务出错时的异常捕获

--- src/app/app.middleware.ts
+++ src/app/app.middleware.ts
@@ -32,6 +32,9 @@ export const defaultErrorHandler = (
     response: Response,
     next: NextFunction
 ) => {
+    if(error.message) {
+        console.log("⚠️", error.message)
+    }
     let statusCode: number, message: string;
 
     /**
