52762b353ca854c4e88da1b58942e3f66ab15421
LongAidenAi <woai032867@gmail.com>
2023-10-12 16:06:10 +0800

    12.09 测试连接数据仓库

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
 import app from './app'
 import { APP_PORT } from './app/app.config'
-
+import {connection} from './app/database/msyql'
 
 app.listen(APP_PORT, () => {
   console.log("服务器启动成功")
+})
+
+/**
+ * 测试使用数据服务连接
+ */
+connection.connect(error => {
+  if(error) {
+    console.log('连接数据服务失败：',error.message)
+  }
+
+  console.log('连接数据服务')
 })
