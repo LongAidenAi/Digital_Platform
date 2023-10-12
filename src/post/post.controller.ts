362c76a6148352dc6761dd1d700f15d2a289865f
LongAidenAi <woai032867@gmail.com>
2023-10-12 19:27:50 +0800

    12.17 安装lodash解决参数选择传入问题

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,6 +1,6 @@
 import { Request,Response, NextFunction } from "express";
 import { getPosts,createPost,updatePost } from "./post.service";
-
+import _ from "lodash"
 
 /**
  * 内容列表
@@ -51,11 +51,18 @@ export const update = async (
     const {postId} = request.params;
 
     //准备数据
-    const {title, content} = request.body
+    /**
+     * 如果使用const {title, content} = request.body
+     * 如果没有在客户端传入title，或者content，则会报错
+     * 是因为如果没有title, 就是{"title": null, "content": "有内容"}
+     * 那么在service层中，执行sql语句就成了update post set "title" = null, "content": "有内容" where id = xx
+     * 在数据库中设置的是，不允许title字段为空的，所以会报错
+     */
+    const post = _.pick(request.body, ['title', 'content'])
 
     //更新内容
     try {
-        const data = await updatePost(parseInt(postId, 10), {title, content})
+        const data = await updatePost(parseInt(postId, 10), post)
         response.status(201).send(data)
     } catch (error) {
         next(error)
