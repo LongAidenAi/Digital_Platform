ecda5c1694d22367879074fa56bed3eaea1041fc
LongAidenAi <woai032867@gmail.com>
2023-10-12 19:12:07 +0800

    12.16 测试更新内容接口

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,5 +1,5 @@
 import { Request,Response, NextFunction } from "express";
-import { getPosts } from "./post.service";
+import { getPosts,createPost,updatePost } from "./post.service";
 
 
 /**
@@ -14,6 +14,49 @@ export const index = async(
     try {
         const posts = await getPosts()
         response.send(posts)
+    } catch (error) {
+        next(error)
+    }
+}
+
+/***
+ * 创建内容
+ */
+export const store = async (
+    request: Request,
+    response: Response,
+    next: NextFunction
+) => {
+    //准备数据
+    const { title, content } = request.body;
+
+    //创建内容
+    try {
+        const data = await createPost({title,content})
+        response.status(201).send(data)
+    } catch (error) {
+        next(error)
+    }
+}
+
+/**
+ * 更新内容
+ */
+export const update = async (
+    request: Request,
+    response: Response,
+    next: NextFunction
+) => {
+    //获取内容ID
+    const {postId} = request.params;
+
+    //准备数据
+    const {title, content} = request.body
+
+    //更新内容
+    try {
+        const data = await updatePost(parseInt(postId, 10), {title, content})
+        response.status(201).send(data)
     } catch (error) {
         next(error)
     }
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,4 +1,5 @@
 import { connection } from "../app/database/msyql"
+import { PostModel } from "./model"
 
 
 
@@ -20,5 +21,37 @@ export const getPosts = async () => {
         on user.id = post.userId
     `
     const [ data ] = await connection.promise().query(statement)
+    return data
+}
+
+/**
+ * 创建内容
+ */
+export const createPost = async (post: PostModel) => {
+    //准备sql
+    const statement = `
+        insert into post 
+        set ?
+    `;
+
+    //执行sql
+    const [data] = await connection.promise().query(statement, post)
+
+    //提供数据
+    return data;
+}
+
+/**
+ * 更新内容
+ */
+export const updatePost = async (postId: number, post: PostModel) => {
+    const statement = `
+        update post
+        set ? 
+        where id = ? 
+    `
+
+    const [ data ] = await connection.promise().query(statement, [post, postId])
+
     return data
 }
