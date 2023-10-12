50262879dad25588529b59576df55277230ef287
LongAidenAi <woai032867@gmail.com>
2023-10-12 20:46:30 +0800

    13.05 测试创建用户接口

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,2 +1,16 @@
 import {connection} from "../app/database/msyql"
 import { UserModel } from "./user.model" 
+
+/**
+ * 创建用户
+ */
+export const createUser = async (user: UserModel) => {
+    const statement = `
+      insert into user 
+      set ?
+    `
+
+    const [ data ] = await connection.promise().query(statement, user)
+
+    return data
+}
