b2bf566aeafd7cbd8cf545f71e7de73658203774
LongAidenAi <woai032867@gmail.com>
2023-10-13 16:01:47 +0800

    14.08 验证用户密码是否匹配

--- src/auth/auth.middleware.ts
+++ src/auth/auth.middleware.ts
@@ -16,11 +16,11 @@ export const validateLoginData = async (
     //准备数据
     const { name, password } = request.body
 
-    //验证必填数据
+    //验证用户名和密码是否为空
     if (!name) return next(new Error('NAME_IS_REQUIRED'))
     if (!password) return next(new Error('PASSWORD_IS_REQUIRED'))
 
-    //验证用户名
+    //验证用户名是否不存在
     const user = await userService.getUserByName(name, {password: true})
     if(!user) return next(new Error('USER_DOES_NOT_EXIST'))
 
