d21d799de232784e9fa5dee58bbbd071e68bda44
LongAidenAi <woai032867@gmail.com>
2023-10-18 18:58:31 +0800

    18.07处理文件 调整图像文件的大小

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -1,3 +1,5 @@
+import path from "path";
+import Jimp from "jimp";
 import { connection } from "../app/database/msyql";
 import { FileModel } from './file.model'
 
@@ -34,4 +36,41 @@ export const findFileById = async (
 
     const [data] = await connection.promise().query(statement, fileId)
     return data[0]
+}
+
+/***
+ * 调整图像尺寸
+ */
+export const imageResizer = async (
+    image: Jimp, file: Express.Multer.File
+) => {
+    //图像尺寸
+    const {imageSize} = image['_exif']
+
+    //文件路径
+    const filePath = path.join(file.destination, 'resized', file.filename)
+
+    //大尺寸
+    if (imageSize.width > 1280) {
+        image
+        .resize(1280, Jimp.AUTO)
+        .quality(85)
+        .write(`${filePath}-large`)
+    }
+
+    //中尺寸
+    if (imageSize.width > 640) {
+        image
+        .resize(640, Jimp.AUTO)
+        .quality(85)
+        .write(`${filePath}-medium`)
+    }
+
+    //中尺寸
+    if (imageSize.width > 320) {
+        image
+        .resize(320, Jimp.AUTO)
+        .quality(85)
+        .write(`${filePath}-thumbnail`)
+    }
 }
