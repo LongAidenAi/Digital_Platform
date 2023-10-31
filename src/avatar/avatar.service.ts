25857ae0462086ecaf6a3323360c08abebe2ae36
LongAidenAi <woai032867@gmail.com>
2023-10-31 23:40:27 +0800

    24.11 保存头像数据

--- src/avatar/avatar.service.ts
+++ src/avatar/avatar.service.ts
@@ -1,1 +1,16 @@
 import { connection } from '../app/database/mysql'
+import { AvatarModel } from './avatar.model'
+
+/**
+ * 保存头像文件信息
+ */
+export const createAvatar = async (avatar: AvatarModel) => {
+    const statement = `
+     insert into avatar
+     set ? 
+    `
+
+    const [data] = await connection.promise().query(statement, avatar)
+
+    return data
+}
