ead67d0f728cab8a0832c68f1e1c390c68b053be
LongAidenAi <woai032867@gmail.com>
2023-10-31 18:14:07 +0800

    24.07 测试上传头像接口

--- src/avatar/avatar.router.ts
+++ src/avatar/avatar.router.ts
@@ -1,6 +1,14 @@
 import express from 'express'
-import * as savatarController from './avatar.controller'
+import * as avatarController from './avatar.controller'
+import {authGuard} from '../auth/auth.middleware'
+import { avatarInterceptor } from './avatar.middleware'
+
 
 const router = express.Router()
+
+/**
+ * 上传文件
+ */
+router.post('/avatar', authGuard, avatarInterceptor, avatarController.store)
 
 export default router
